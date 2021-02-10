4fd5e1d5c8b6f170bc79714ea62b8e1a57a6733f
Ayoub Elmendoub <76922728+MotivatedDesigner@users.noreply.github.com>
2021-02-10 15:16:13 +0100

    testing scroll library

--- js/app.js
+++ js/app.js
@@ -1,10 +1,9 @@
 new fullpage('#fullpage',{
     autoScrolling:true,
-    anchors: ['s1', 's2'],
+    anchors: ['header', 'about-me'],
     responsive: 900,
     navigation: true,
     navigationPosition: 'right',
     navigationTooltips: ['Section 0', 'Section 1'],
-    menu:'.nav-list',
-    css3: true,
+    menu:'#menu',
 });
