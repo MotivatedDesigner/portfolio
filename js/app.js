db9b2772c9d3f8204cf84b6b265230370932e3d3
Ayoub Elmendoub <76922728+MotivatedDesigner@users.noreply.github.com>
2021-02-10 15:11:32 +0100

    adding fullpage scroll

---
+++ js/app.js
@@ -0,0 +1,10 @@
+new fullpage('#fullpage',{
+    autoScrolling:true,
+    anchors: ['s1', 's2'],
+    responsive: 900,
+    navigation: true,
+    navigationPosition: 'right',
+    navigationTooltips: ['Section 0', 'Section 1'],
+    menu:'.nav-list',
+    css3: true,
+});
